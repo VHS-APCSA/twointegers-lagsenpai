889b258dc46ce58b3d0029d8c952a5e638b7ff23
bob <bob@APCSA>
2018-09-04 15:37:15 -0700

    Added user input for numbers

---
+++ src/TwoIntegersRunner.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class TwoIntegersRunner {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter number: ");
+		int value = input.nextInt();
+		System.out.print("Enter another number: ");
+		int value2 = input.nextInt();
+		System.out.print(value + " + " + value2);
+
+		
+		
+
+	}
+
+}
