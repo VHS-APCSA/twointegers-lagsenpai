a3a440e05375ae722daf78893263efdc10399161
bob <bob@APCSA>
2018-09-06 15:14:35 -0700

    Final commit

--- src/TwoIntegersRunner.java
+++ src/TwoIntegersRunner.java
@@ -7,15 +7,15 @@ public class TwoIntegersRunner {
 		int value = input.nextInt();
 		System.out.print("Enter another number: ");
 		int value2 = input.nextInt();
-		//System.out.print(value + " + " + value2);
 
 		TwoIntegers twointegers = new TwoIntegers(value, value2);
 		String arithmetic = twointegers.arithmetic();
+		int largernum = twointegers.larger();
 		boolean multiple = twointegers.isMultiple();
 		
-		System.out.println(multiple);
-		
-		
+		System.out.println(arithmetic);
+		System.out.println(largernum + " is the larger number.");
+		System.out.println("Is " + value + " a multiple of " + value2 + ": " + multiple);
 
 	}
 
